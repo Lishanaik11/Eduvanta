fcc7ca805f9ccef0ca636ecd637b5cee0d43517c
Naik Lisha Prabhakar <lishanaik11@gmail.com>
2026-05-24 18:43:04 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,20 +1,5 @@
-import pool from "../db.js";
+import app from "../server.js";
 
-export default async function handler(req, res) {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    
-    return res.status(200).json({
-      success: true,
-      time: result.rows[0]
-    });
-
-  } catch (err) {
-    console.error("DB ERROR:", err);
-
-    return res.status(500).json({
-      success: false,
-      error: err.message
-    });
-  }
+export default function handler(req, res) {
+  return app(req, res);
 }
