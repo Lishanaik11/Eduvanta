428943e966c585c4f974784c4fbda1586aaaa9f5
Lisha Naik <lishanaik11@gmail.com>
2026-05-25 13:03:23 +0530

    db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,16 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
