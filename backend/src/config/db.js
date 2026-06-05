395bafce81df1f1390ef2e60ad58a32fa7eb1033
Lisha Naik <lishanaik11@gmail.com>
2026-06-05 16:10:45 +0530

    new changes for deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,11 +6,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
