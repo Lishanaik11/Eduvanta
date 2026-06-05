4fa7645d82c0d63a8a0b3e0de62cf78b9d3caae5
Naik Lisha Prabhakar <lishanaik11@gmail.com>
2026-06-05 16:03:04 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -90,13 +90,11 @@ app.use(
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`=========================================`);
-    console.log(`🚀 Server listening on: http://localhost:${PORT}`);
-    console.log(`=========================================`);
-  });
-}
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`=========================================`);
+  console.log(`🚀 Server listening on port ${PORT}`);
+  console.log(`=========================================`);
+});
 
 
 
